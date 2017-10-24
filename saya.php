cd69da25c24e63bc134fa4b1943a0b0b39401e4c
unknown <171610132@smkassalaambandung.sch.id>
2017-10-24 09:00:30 +0700

    tugas kedua saya

---
+++ saya.php
@@ -0,0 +1,13 @@
+<?php
+
+class melan{
+
+ 	//property
+ 	public $berat = 41;
+ 	public $sifat = 'baik hati';
+ }
+
+ $melan1 = new melan;
+ echo "Melan itu memiliki berat " .$melan1-> berat.' kg ','<br>';
+ echo "Melan itu memiliki sifat yang sangat" .$melan1-> sifat.'<br>'; 
+?>
---
+++ teman.php
@@ -0,0 +1,13 @@
+<?php
+
+class teman{
+
+ 	//property
+ 	public $wajah = 'cantik';
+ 	public $sifat = 'baik';
+ }
+
+ $teman1 = new teman;
+ echo "teman saya itu memiliki wajah " .$teman1-> wajah. '<br>';
+ echo "teman saya itu memiliki sifat yang sangat" .$teman1-> sifat.'<br>'; 
+?>
